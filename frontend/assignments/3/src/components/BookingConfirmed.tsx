d21ccfb6693415b4a28b1865d3e0efb63aa3c4fa
kajalpandey-pixel <kajal.pandey@kickdrumtech.com>
2026-02-16 23:09:50 +0530

    Delete frontend/assignments/3/src directory

--- frontend/assignments/3/src/components/BookingConfirmed.tsx
+++
@@ -1,8 +0,0 @@
-export default function BookingConfirmed() {
-  return (
-    <div style={{ marginTop: 16, padding: 12, border: "1px solid green" }}>
-      <h2>BOOKING CONFIRMED</h2>
-      <p>Your selections are shown in the summary above.</p>
-    </div>
-  );
-}
